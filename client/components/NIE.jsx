db6179ef6f1a3db9f79c743b12fb116bf4dcfb93
AliBayramDC <ali.abbayram@gmail.com>
2023-10-21 19:53:12 +0400

    msg1

---
+++ client/components/NIE.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// const NIE = () => {
+//   return (
+//     <div>
+        
+//     </div>
+//   )
+// }
+
+// export default NIE
---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import About from './components/Tərəfdaşlar';
+import BizimBlog from './components/BizimBlog';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar'
+import ScrollComponent from './components/ScrollComponent';
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <ScrollComponent/>
+      <BizimBlog />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
